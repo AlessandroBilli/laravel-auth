7effb1ec4566622c2e1151e4be9b2d755d747abd
AlessandroBilli <skillmanbungo@gmail.com>
2024-02-06 23:54:20 +0100

    feat: struttura stile dashboard pt 2

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,6 +4,8 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8 mt-4">
+                <h1>Boolfolio</h1>
+                <p class="fs-2 ">I tuoi progetti a portata di click:</p>
                 <div class="card">
                     <div class="card-header">{{ __('Portfolio') }}</div>
 
